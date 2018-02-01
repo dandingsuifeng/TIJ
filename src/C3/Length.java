15d38cf0089714c04d94f62c49bbe37fe8b5294c
dandingsuifeng <dandingsuifeng@gmail.com>
2018-02-01 23:40:43 +0800

    百度试题2（二星）

--- src/C3/Length.java
+++ src/C3/Length.java
@@ -9,9 +9,28 @@ public class Length {
         int temp = 0;
         int[] point  = new  int[N];
         int[] alength  = new  int[N];
+        alength[0] = 0;
         for (int i = 0;i<N;i++){
             point[i] = sc.nextInt();
         }
-
+        int length = 0;
+        int min = 999;
+        for (int i=1;i<N-1;i++){
+            if (N==2){
+                if(i==1){
+                    alength[i]=Math.abs(point[i]-point[i-1]);
+                }
+            }
+            else {
+                for (int j = 0;j<N;j++){
+                    while (j!=i&&j!=i+1){
+                        length+=Math.abs(point[j]-point[j-1]);
+                    }
+                    if(j==i+1){
+                        length+=Math.abs(point[j]-point[j-2]);
+                    }
+                }
+            }
+        }
     }
 }
